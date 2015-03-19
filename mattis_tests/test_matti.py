0171fc78246b24920efb46941d2ced2c46ac2e34
Matti Jokitulppo <g8049@student.jamk.fi>
2015-03-19 21:49:00 +0200

    simple http method test case

--- mattis_tests/test_matti.py
+++ mattis_tests/test_matti.py
@@ -12,10 +12,10 @@ import requests
 import pytest
 import unittest
 
-class HTTPMethodTestCase(unittest.TestCase):
+class SimpleHTTPMethodTestCase(unittest.TestCase):
 
     def test_simple_get(self):
-        r = requests.get("http://google.com")
+        r = requests.get("http://httpbin.org/encoding/utf8")
         assert r.status_code == 200
         assert r.headers['content-type'] == 'text/html; charset=UTF-8'
 
@@ -24,6 +24,25 @@ class HTTPMethodTestCase(unittest.TestCase):
         r = requests.post("http://httpbin.org/post", params=payload)
         assert r.status_code == 200
         assert "testkey" in r.text
+
+    def test_simple_delete(self):
+        r = requests.delete("http://httpbin.org/delete")
+        assert r.status_code == 200
+
+    def test_simple_put(self):
+        payload = {'testkey':'testvalue'}
+        r = requests.put("http://httpbin.org/put", params=payload)
+        assert r.status_code == 200
+        assert "testkey" in r.text
+
+    def test_simple_head(self):
+        r = requests.head("http://httpbin.org/head")
+        assert r.status_code == 200
+
+    def test_simple_options(self):
+        r = requests.options("http://httpbin.org/post")
+        assert r.status_code == 200
+
 
 if __name__ == '__main__':
     unittest.main()
